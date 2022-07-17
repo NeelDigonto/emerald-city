558a1ee9741d6c2416b349fe838c9a46b0c95e9c
Saikat Dey <digonto.pvt.2021@gmail.com>
2022-07-17 22:57:23 +0530

    Setup dotenv

--- emerald-city-backend/src/main.ts
+++ emerald-city-backend/src/main.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { getMongoConnection } from './util/db.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
+
+console.log(process.env.S3_BUCKET);
 
 app.post('/get-presigned-post-urls', (req, res) => {
   res.send({ wow: 'meow' });
