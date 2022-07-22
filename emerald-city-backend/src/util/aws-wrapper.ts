0fc155b62cbba0230874d7ec95b18fbb16a4c022
Saikat Dey <digonto.pvt.2021@gmail.com>
2022-07-23 02:15:25 +0530

    Added aws-wrapper for s3

---
+++ emerald-city-backend/src/util/aws-wrapper.ts
@@ -0,0 +1,67 @@
+import {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} from '@aws-sdk/client-s3';
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { response } from 'express';
+import fs, { write } from 'fs';
+import { Readable } from 'stream';
+
+export namespace s3 {
+  export async function getObject(bucket: string, region: string, key: string) {
+    const client = new S3Client({ region: region, apiVersion: '2006-03-01' });
+    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+    return client.send(command);
+  }
+
+  export async function putObject(
+    bucket: string,
+    region: string,
+    key: string,
+    file: Readable,
+  ) {
+    const client = new S3Client({ region: region, apiVersion: '2006-03-01' });
+    const command = new PutObjectCommand({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+    });
+    return client.send(command);
+  }
+
+  export async function getPresignedGetUrl(
+    bucket: string,
+    region: string,
+    key: string,
+    expires: number = 10 * 60,
+  ) {
+    const client = new S3Client({ region: region, apiVersion: '2006-03-01' });
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    });
+
+    const url = await getSignedUrl(client, command, { expiresIn: expires });
+    return url;
+  }
+
+  export async function getPresignedPostUrl(
+    bucket: string,
+    region: string,
+    key: string,
+    expires: number = 10 * 60,
+  ) {
+    const Fields = {
+      acl: 'public-read',
+    };
+    const client = new S3Client({ region: region, apiVersion: '2006-03-01' });
+    return createPresignedPost(client, {
+      Bucket: bucket,
+      Key: key,
+      Fields,
+      Expires: expires,
+    });
+  }
+}
